32ba9add5ce18096d3459f12bd6960032eab0241
valentin.hetriuc <valentin.hetriuc@gmail.com>
2023-02-27 19:54:34 +0200

    feat: refactor function to arrow function

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,13 +1,17 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
-export default function Document() {
+const Document = () => {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="manifest" href="/manifest.webmanifest" />
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
-}
+  );
+};
+
+export default Document;
