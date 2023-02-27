0f68b31d8c700bb232303ff6ba7896bb8c69f6c4
valentin.hetriuc <valentin.hetriuc@gmail.com>
2023-02-27 22:41:13 +0200

    feat: add new fields to manifest

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -4,6 +4,13 @@ const Document = () => (
   <Html lang="en">
     <Head>
       <link rel="manifest" href="/manifest.webmanifest" />
+      <link rel="apple-touch-icon" href="/icons/ios/icon.png" />
+      -- meta for change theme color
+      <meta
+        name="theme-color"
+        content="#ffffff"
+        media="(prefers-color-scheme: dark)"
+      />
     </Head>
     <body>
       <Main />
