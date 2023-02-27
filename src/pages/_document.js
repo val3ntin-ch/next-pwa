1a8374bc2ec87a33f27be54b4336fe763c8ffa58
valentin.hetriuc <valentin.hetriuc@gmail.com>
2023-02-27 19:59:23 +0200

    feat: add manifest file

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,17 +1,15 @@
 import { Html, Head, Main, NextScript } from "next/document";
 
-const Document = () => {
-  return (
-    <Html lang="en">
-      <Head>
-        <link rel="manifest" href="/manifest.webmanifest" />
-      </Head>
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
-};
+const Document = () => (
+  <Html lang="en">
+    <Head>
+      <link rel="manifest" href="/manifest.webmanifest" />
+    </Head>
+    <body>
+      <Main />
+      <NextScript />
+    </body>
+  </Html>
+);
 
 export default Document;
